4fb658578afe16220e9d9805f5b778754ee774d6
Raj Panchal <roger71011112000@gmail.com>
2023-11-07 13:30:27 -0600

    Changes made to Main file

--- a4_main.cpp
+++ a4_main.cpp
@@ -1,12 +1,25 @@
+/**
+1) Module Name: a4_main.cpp
+2) Author: Raj Panchal (rjp659@uregina.ca)
+3) Date of creation: 2023-11-06
+4) Purpose:This is the file which consists of the main function from where the compilation of the program commences.
+*/
+
 #include "a4_shakersortutils.cpp" 
 
+/**
+1) Purpose:This is the main function from where the program's compilation begins and various user defined functions are invoked here based on the requirement.
+2) Parameters: None
+3) Return type: int
+4) Side Effect: None
+*/
 int main() 
 {
     int n;
     cout << "Enter the size of the vector: ";
     cin >> n;
 
-    // Generate a vector of random nums
+    //! Generates a vector of random nums
     vector<int> nums;
     srand(time(0));
 
@@ -15,7 +28,7 @@ int main()
         nums.push_back(rand() % 10000);
     }
 
-    // Save the generated vector to a file
+    //! Saves the generated vector to a file
     ofstream inputFile("input_file.txt");
 
     for (int num : nums) 
@@ -25,14 +38,14 @@ int main()
 
     inputFile.close();
 
-    // Sort the vector in asc order
+    //! Sorts the vector in ascending order
     vector<int> ascnums = nums;
     clock_t startTime = clock();
     shakerSort(ascnums, true);
     clock_t endTime = clock();
-    double exetimeasc = static_cast<double>(endTime - startTime) / CLOCKS_PER_SEC;
+    double exetimeasc = static_cast<double>(endTime - startTime) / CLOCKS_PER_SEC; //! Calculates the time taken to sort the vector in ascending order
 
-    // Save the sorted asc vector to a file
+    //! Saves the sorted ascending vector to a file
     ofstream ascFile("asc_sorted.txt");
 
     for (int num : ascnums)
@@ -41,14 +54,14 @@ int main()
     }
     ascFile.close();
 
-    // Sort the vector in desc order
+    //! Sorts the vector in descending order
     vector<int> descnums = nums;
     startTime = clock();
     shakerSort(descnums, false);
     endTime = clock();
-    double exetimedesc = static_cast<double>(endTime - startTime) / CLOCKS_PER_SEC;
+    double exetimedesc = static_cast<double>(endTime - startTime) / CLOCKS_PER_SEC; //! Calculates the time taken to sort the vector in descending order
 
-    // Save the sorted desc vector to a file
+    //! Saves the sorted descending vector to a file
     ofstream descFile("desc_sorted.txt");
 
     for (int num : descnums)
@@ -57,8 +70,8 @@ int main()
     }
     descFile.close();
 
-    cout << "\nTime taken by 'Shaker Sort' to sort the array in ascending order  : " << fixed << setprecision(3) << exetimeasc << " seconds" << endl;
-    cout << "\nTime taken by 'Shaker Sort' to sort the array in descending order : " << fixed << setprecision(3) << exetimedesc << " seconds" << endl;
+    cout << "\nTime taken by 'Shaker Sort' to sort the vector in ascending order  : " << fixed << setprecision(3) << exetimeasc << " seconds" << endl;
+    cout << "\nTime taken by 'Shaker Sort' to sort the vector in descending order : " << fixed << setprecision(3) << exetimedesc << " seconds" << endl;
     cout << "\nSorted files for both ascending and descending order has been created and saved successfully !!!\n\n";
     
     return 0;
