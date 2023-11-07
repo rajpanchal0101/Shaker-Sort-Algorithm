956cef9807b8a190820f8323a9be6a5a10d6b66d
Raj Panchal <roger71011112000@gmail.com>
2023-11-07 13:59:53 -0600

    Internal documentation added

--- a4_shakersortutils.cpp
+++ a4_shakersortutils.cpp
@@ -1,6 +1,13 @@
+/**
+1) Module Name: a4_shakersortutils.cpp
+2) Author: Raj Panchal (rjp659@uregina.ca)
+3) Date of creation: 2023-11-07
+4) Purpose:This is the file which consists of implementation of the various functions declared in the header file.
+*/
+
 #include "a4_header.h"
 
-// Shaker Sort function to sort the vector in asc or desc order
+//! Shaker Sort function to sort the vector in ascending or descending order
 void shakerSort(vector<int> &arr, bool asc)
 {
     int n = arr.size();
@@ -16,14 +23,14 @@ void shakerSort(vector<int> &arr, bool asc)
             {
                 if (arr[i] > arr[i + 1]) 
                 {
-                    swap(arr[i], arr[i + 1]);
+                    swap(arr[i], arr[i + 1]); //! Swaps elements if they are out of order
                     swapped = true;
                 }
             }
 
             n--;
 
-            if (!swapped) break;
+            if (!swapped) break;  //! If no elements were swapped, the array is sorted
 
             swapped = false;
 
@@ -31,7 +38,7 @@ void shakerSort(vector<int> &arr, bool asc)
             {
                 if (arr[i] < arr[i - 1]) 
                 {
-                    swap(arr[i], arr[i - 1]);
+                    swap(arr[i], arr[i - 1]); //! Swaps elements if they are out of order
                     swapped = true;
                 }
             }
@@ -43,14 +50,14 @@ void shakerSort(vector<int> &arr, bool asc)
             {
                 if (arr[i] < arr[i + 1]) 
                 {
-                    swap(arr[i], arr[i + 1]);
+                    swap(arr[i], arr[i + 1]); //! Swaps elements if they are out of order
                     swapped = true;
                 }
             }
 
             n--;
 
-            if (!swapped) break;
+            if (!swapped) break;  //! If no elements were swapped, the array is sorted
 
             swapped = false;
 
@@ -58,7 +65,7 @@ void shakerSort(vector<int> &arr, bool asc)
             {
                 if (arr[i] > arr[i - 1])
                 {
-                    swap(arr[i], arr[i - 1]);
+                    swap(arr[i], arr[i - 1]); //! Swaps elements if they are out of order
                     swapped = true;
                 }
             }
